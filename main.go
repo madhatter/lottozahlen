8cb2359dd4666f7ca4f1d794771967eee9452daf
Arvid Warnecke <madhatter@nostalgix.org>
2019-08-01 22:13:27 +0200

    Using a simpler approach to generate the numbers

--- main.go
+++ main.go
@@ -10,33 +10,28 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func generateNumbers(n int, l int) []int {
-	winningNumbers := make([]int, n)
-	usedNumbers := make(map[int]struct{})
+func createSlice(max int) []int {
+	min := 1
+	arr := make([]int, max)
 
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-
-	//for len(winningNumbers) < n {
-	for i := range winningNumbers {
-		var num int
-		for {
-			num = r1.Intn(l)
-
-			if _, ok := usedNumbers[num]; !ok {
-				break
-			}
-		}
-		usedNumbers[num] = struct{}{}
-		winningNumbers[i] = num
+	for i := range arr {
+		arr[i] = min + i
 	}
+	return arr
+}
 
-	return winningNumbers
+func generateNumbers(n int, l int) []int {
+	numbrs := createSlice(l)
+	rand.Shuffle(len(numbrs), func(i, j int) { numbrs[i], numbrs[j] = numbrs[j], numbrs[i] })
+
+	return numbrs[0:n]
 }
 
 func main() {
 	arg, _ := strconv.Atoi(os.Args[1])
 	log.SetFormatter(&log.JSONFormatter{})
+	rand.Seed(time.Now().UnixNano())
+
 	fmt.Printf("Set of %v.\n", arg)
 	for i := 1; i < arg+1; i++ {
 		fmt.Println(generateNumbers(5, 50))
